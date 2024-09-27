8b65ae9bad597e13eb5a57159d8fdd164ea1a632
Jonatan Pereyra <jpereyraacevedo@gmail.com>
2024-09-27 10:11:00 -0300

    search bar functional

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -7,8 +7,8 @@ export default function () {
     const [title, setTitle] = useState("OBRA")
 
     return (
-        <div className="mx-3">
-            <section className="rounded border flex-col max-w-5xl mx-auto mt-5 mb-10 bg-white container z-10 flex justify-center items-center">
+        <div className="mx-3 altura-minima">
+            <section className="rounded border flex-col max-w-5xl mx-auto mt-5 mb-10 bg-white container z-10 flex justify-center items-center altura-minima">
                 <div className="flex align-center content-center">
                     <hr />
                     <div className="flex align-center content-center justify-center">
--- app/components/InputContainer/InputContainer.js
+++ app/components/InputContainer/InputContainer.js
@@ -17,6 +17,7 @@ export default function InputContainer({ title }) {
         setHasSearched(true); // Marcar como que el usuario ha buscado
     };
 
+    // Esta funcion se conecta al backend
     const getData = async () => {
         try {
             const response = await axios.get("http://localhost:5000/routes/data");
@@ -29,8 +30,9 @@ export default function InputContainer({ title }) {
     // Filtramos los datos de la búsqueda
     useEffect(() => {
         if (busqueda.trim() === "") {
-            // Si no hay búsqueda, no mostramos nada si el usuario ha buscado algo antes
+            // Si no hay búsqueda, restablecemos las filas vacías
             setPinturas([]);
+            setHasSearched(false); // Restablecer el estado de búsqueda
         } else {
             // Filtramos los datos según la búsqueda
             const resultadosBusqueda = tablaPinturas.filter((elemento) =>
@@ -60,7 +62,7 @@ export default function InputContainer({ title }) {
     const emptyRows = minRows - pinturas.length > 0 ? new Array(minRows - pinturas.length).fill({}) : [];
 
     return (
-        <div className="mb-10">
+        <div className="mb-10 ancho-minimo">
             <h2 className="my-2 text-3xl text-center text-[#0154b8] font-bold">{title}</h2>
             <div className="flex flex-col m-2 items-center justify-center">
                 <label>
@@ -77,7 +79,7 @@ export default function InputContainer({ title }) {
             <hr />
             <Card className="w-full">
                 {/* Encabezado fijo siempre visible */}
-                <div className="grid grid-cols-4 gap-4 text-left p-2">
+                <div className="grid grid-cols-4 gap-4 text-left p-2 min-w-full">
                     <div className="font-bold text-[#0154b8]">Col</div>
                     <div className="font-bold text-[#0154b8]">Cantidad</div>
                     <div className="font-bold text-[#0154b8]">Precio</div>
@@ -91,7 +93,7 @@ export default function InputContainer({ title }) {
                         [...pinturas, ...emptyRows].map((pintura, index) => (
                             <div
                                 className={`grid grid-cols-4 gap-4 p-2 items-center zebra-row ${index % 2 === 0 ? 'bg-white' : 'bg-gray-200'}`}
-                                key={pintura.Id || index} // Usar el índice para filas vacías
+                                key={pintura.Id || `empty-${index}`} // Asegurarse de que las claves sean únicas
                                 style={{ minHeight: "45px" }} // Ajusta esta altura según necesites
                             >
                                 <div style={{ minWidth: "120px" }}> {/* Fija el ancho de esta columna */}
@@ -133,8 +135,17 @@ export default function InputContainer({ title }) {
                     )}
                 </div>
             </Card>
-            <div className="flex justify-between p-5 border-t border-blue-gray-100">
-                {/* Agregar cualquier otro contenido aquí */}
+            <div className="flex justify-between p-5 border-t border-blue-gray-100 text-[#0154b8]">
+                <div className="flex flex-col">
+                    <p className="font-bold">Importe colorante</p>
+                    <p className="font-bold">Importe base</p>
+                    <p className="font-bold">Importe TOTAL</p>
+                </div>
+                <div className="flex flex-col">
+                    <p className="font-bold">$</p>
+                    <p className="font-bold">$</p>
+                    <p className="font-bold">$</p>
+                </div>
             </div>
         </div>
     );
