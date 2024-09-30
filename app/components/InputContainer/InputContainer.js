ef5a49d637b244505b2c42e52170e9a83305bdf8
Jonatan Pereyra <jpereyraacevedo@gmail.com>
2024-09-30 12:19:39 -0300

    bases working

--- app/components/InputContainer/InputContainer.js
+++ app/components/InputContainer/InputContainer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Card, Typography, Input } from "@material-tailwind/react";
+import { Card, Typography, Input, Button } from "@material-tailwind/react";
 import { ClassContext } from "../../Context";
 import axios from "axios";
 import "./InputContainer.css"; // Asegúrate de agregar los estilos nuevos aquí.
@@ -7,14 +7,22 @@ import "./InputContainer.css"; // Asegúrate de agregar los estilos nuevos aquí
 export default function InputContainer() {
     const { footerActive } = useContext(ClassContext);
     const [pinturas, setPinturas] = useState([]);
-    const [tablaPinturas, setTablaPinturas] = useState([]);
+    const [tablaPinturas, setTablaPinturas] = useState([]); // Datos originales de la base de datos
     const [busqueda, setBusqueda] = useState("");
+    const [bases, setBases] = useState([]); // Para almacenar las bases coincidentes
+    const [selectedBase, setSelectedBase] = useState(null); // Base seleccionada para llenar campos
     const [hasSearched, setHasSearched] = useState(false); // Estado para controlar si el usuario ha buscado
 
     const handleInputChange = (event) => {
         const value = event.target.value;
         setBusqueda(value);
-        setHasSearched(true); // Marcar como que el usuario ha buscado
+
+        // Si se borra la búsqueda, restablecer el estado
+        if (value === "") {
+            setBases([]);
+            setSelectedBase(null);
+            setHasSearched(false); // No se ha buscado aún
+        }
     };
 
     // Esta funcion se conecta al backend
@@ -22,31 +30,24 @@ export default function InputContainer() {
         try {
             const response = await axios.get("http://localhost:5000/routes/data");
             setTablaPinturas(response.data); // Guardamos los datos originales
+            console.log("Datos recibidos desde SQL:", response.data);
         } catch (err) {
             console.log(err);
         }
     };
 
-    // Filtramos los datos de la búsqueda
-    useEffect(() => {
-        if (busqueda.trim() === "") {
-            // Si no hay búsqueda, restablecemos las filas vacías
-            setPinturas([]);
-            setHasSearched(false); // Restablecer el estado de búsqueda
-        } else {
-            // Filtramos los datos según la búsqueda
-            const resultadosBusqueda = tablaPinturas.filter((elemento) =>
-                elemento.Col.toLowerCase().includes(busqueda.toLowerCase())
-            );
-            setPinturas(resultadosBusqueda);
-        }
-    }, [busqueda, tablaPinturas]);
-
-    const handleEditableChange = (id, field, value) => {
-        const updatedPinturas = pinturas.map(pintura =>
-            pintura.Id === id ? { ...pintura, [field]: value } : pintura
+    // Filtramos los valores de Base al buscar
+    const buscarBases = () => {
+        const resultadosBusqueda = tablaPinturas.filter((elemento) =>
+            elemento.Codigo.toLowerCase() === busqueda.toLowerCase()
         );
-        setPinturas(updatedPinturas);
+        setBases(resultadosBusqueda.map(item => item.Base)); // Guardamos las bases coincidentes
+        setHasSearched(true); // Marcar que se ha buscado
+    };
+
+    const handleBaseSelect = (base) => {
+        const selectedPintura = tablaPinturas.find(item => item.Base === base);
+        setSelectedBase(selectedPintura);
     };
 
     useEffect(() => {
@@ -57,9 +58,29 @@ export default function InputContainer() {
         footerActive(pinturas.length === 0);
     }, [pinturas]);
 
-    // Asegurarse de que siempre haya al menos 8 filas
-    const minRows = 8;
-    const emptyRows = minRows - pinturas.length > 0 ? new Array(minRows - pinturas.length).fill({}) : [];
+    // Generar filas vacías para la tabla
+    const generarFilasVacias = (num) => {
+        return Array.from({ length: num }, (_, index) => (
+            <div key={index} className={`grid grid-cols-4 gap-4 p-2 items-center zebra-row`}>
+                <div style={{ minWidth: "120px" }}>
+                    <div className="text-[#0154b8] border rounded p-1" style={{ height: "30px" }} />
+                </div>
+                <div style={{ minWidth: "100px" }}>
+                    <div className="text-[#0154b8] border rounded p-1" style={{ height: "30px" }} />
+                </div>
+                <div style={{ minWidth: "100px" }}>
+                    <Typography variant="small" className="font-bold text-[#0154b8]">
+                        {""}
+                    </Typography>
+                </div>
+                <div style={{ minWidth: "100px" }}>
+                    <Typography variant="small" className="font-bold text-[#0154b8]">
+                        {""}
+                    </Typography>
+                </div>
+            </div>
+        ));
+    };
 
     return (
         <div className="mb-10 ancho-minimo">
@@ -74,77 +95,68 @@ export default function InputContainer() {
                         onChange={handleInputChange}
                     />
                 </label>
-                <h2 className="my-2 text-xl text-center text-[#0154b8] font-bold">{busqueda === "" ? " No ha ingresado ningún color" : busqueda}</h2>
+                <Button onClick={buscarBases} className="my-2 bg-[#0154b8]">Buscar</Button>
+            </div>
+            <div className="flex flex-col m-2 items-center justify-center">
+                {hasSearched && bases.length > 0 && (
+                    <select onChange={(e) => handleBaseSelect(e.target.value)} className="rounded input-design my-2 bg-[#efecec]">
+                        <option value="">Seleccione una base</option>
+                        {bases.map((base, index) => (
+                            <option key={index} value={base}>{base}</option>
+                        ))}
+                    </select>
+                )}
             </div>
             <hr />
             <Card className="w-full">
                 {/* Encabezado fijo siempre visible */}
                 <div className="grid grid-cols-4 gap-4 text-left p-2 min-w-full">
-                    <div className="font-bold text-[#0154b8]">Col</div>
+                    <div className="font-bold text-[#0154b8]">Colorante</div>
                     <div className="font-bold text-[#0154b8]">Cantidad</div>
                     <div className="font-bold text-[#0154b8]">Precio</div>
                     <div className="font-bold text-[#0154b8]">Importe</div>
                 </div>
                 <hr />
                 <div>
-                    {hasSearched && pinturas.length === 0 ? (
-                        <div className="p-2 text-center text-gray-500">No se encontraron resultados</div>
-                    ) : (
-                        [...pinturas, ...emptyRows].map((pintura, index) => (
-                            <div
-                                className={`grid grid-cols-4 gap-4 p-2 items-center zebra-row ${index % 2 === 0 ? 'bg-white' : 'bg-gray-200'}`}
-                                key={pintura.Id || `empty-${index}`} // Asegurarse de que las claves sean únicas
-                                style={{ minHeight: "45px" }} // Ajusta esta altura según necesites
-                            >
-                                <div style={{ minWidth: "120px" }}> {/* Fija el ancho de esta columna */}
-                                    {pintura.Id ? (
-                                        <Input
-                                            type="text"
-                                            value={pintura.Col || ""}
-                                            onChange={(e) => handleEditableChange(pintura.Id, "Col", e.target.value)}
-                                            className={`text-[#0154b8] border rounded p-1 ${index % 2 !== 0 ? 'input-gray' : ''}`}
-                                        />
-                                    ) : (
-                                        <div style={{ minHeight: "35px", minWidth: "120px" }}></div>  // Placeholder con el mismo ancho
-                                    )}
-                                </div>
-                                <div style={{ minWidth: "100px" }}> {/* Fija el ancho de esta columna */}
-                                    {pintura.Id ? (
-                                        <Input
-                                            type="text"
-                                            value={pintura.Cant || ""}
-                                            onChange={(e) => handleEditableChange(pintura.Id, "Cant", e.target.value)}
-                                            className={`text-[#0154b8] border rounded p-1 ${index % 2 !== 0 ? 'input-gray' : ''}`}
-                                        />
-                                    ) : (
-                                        <div style={{ minHeight: "35px", minWidth: "100px" }}></div>  // Placeholder con el mismo ancho
-                                    )}
-                                </div>
-                                <div style={{ minWidth: "100px" }}> {/* Fija el ancho de esta columna */}
-                                    <Typography variant="small" className="font-bold text-[#0154b8]">
-                                        {pintura.Precio ? `$${pintura.Precio}` : ""}
-                                    </Typography>
-                                </div>
-                                <div style={{ minWidth: "100px" }}> {/* Fija el ancho de esta columna */}
-                                    <Typography variant="small" className="font-bold text-[#0154b8]">
-                                        {pintura.Importe ? `$${pintura.Importe}` : ""}
-                                    </Typography>
+                    {/* Mostrar datos seleccionados o filas vacías */}
+                    {selectedBase ? (
+                        <div className={`grid grid-cols-4 gap-4 p-2 items-center zebra-row`}>
+                            <div style={{ minWidth: "120px" }}>
+                                <div className="text-[#0154b8] border rounded p-1" style={{ height: "30px" }}>
+                                    {selectedBase.Col || ""}
                                 </div>
                             </div>
-                        ))
+                            <div style={{ minWidth: "100px" }}>
+                                <div className="text-[#0154b8] border rounded p-1" style={{ height: "30px" }}>
+                                    {selectedBase.Cant || ""}
+                                </div>
+                            </div>
+                            <div style={{ minWidth: "100px" }}>
+                                <Typography variant="small" className="font-bold text-[#0154b8]">
+                                    {selectedBase.CodigoBase3 ? `$${selectedBase.CodigoBase3}` : ""}
+                                </Typography>
+                            </div>
+                            <div style={{ minWidth: "100px" }}>
+                                <Typography variant="small" className="font-bold text-[#0154b8]">
+                                    {selectedBase.CodigoBase3 ? `$${selectedBase.CodigoBase3}` : ""}
+                                </Typography>
+                            </div>
+                        </div>
+                    ) : (
+                        generarFilasVacias(8) // Generar 8 filas vacías
                     )}
                 </div>
             </Card>
-            <div className="flex justify-between p-5 border-t border-blue-gray-100 text-[#0154b8]">
+            <div className="flex justify-between p-5 border-t border-blue-gray-100 text-[#0154b8] font-bold">
                 <div className="flex flex-col">
-                    <p className="font-bold">Importe base</p>
-                    <p className="font-bold">Importe colorante</p>
-                    <p className="font-bold">Importe TOTAL</p>
+                    <p>Importe base</p>
+                    <p>Importe colorante</p>
+                    <p>Importe TOTAL</p>
                 </div>
                 <div className="flex flex-col">
-                    <p className="font-bold">$</p>
-                    <p className="font-bold">$</p>
-                    <p className="font-bold">$</p>
+                    <p>$</p>
+                    <p>$</p>
+                    <p>$</p>
                 </div>
             </div>
         </div>
